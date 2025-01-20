e85ca434ea9d1e9eb4b015333ce6e867b00d6ce5
itamarattali <itamarattali@gmail.com>
2025-01-20 21:06:30 +0200

    bug fix, 2 requests with the same path

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
+// Add a New Post
 router.post('/', postController.addPost);
+
+// Get All Posts
 router.get('/', postController.getAllPosts);
+
+// Get Posts by Sender
+router.get('/by-sender', postController.getPostsBySender);
+
+// Get Post by ID
 router.get('/:id', postController.getPostById);
-router.get('/', postController.getPostsBySender);
 
 module.exports = router;
