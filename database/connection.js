ec4f24d94814ec4b40c9cbd978a232dcc9e8a323
itamarattali <itamarattali@gmail.com>
2025-01-21 22:41:16 +0200

    bug fixing

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 const dbPath = process.env.DB_PATH;
 
 mongoose.connect(dbPath, {
--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const postRoutes = require('./routes/postRoutes');
 const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
+const bodyParser = require('body-parser');
+
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true, limit: '1mb'}));
+app.use(bodyParser.json());
 app.use('/posts', postRoutes);
 
 app.listen(port, () => {
