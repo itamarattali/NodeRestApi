2ea163387033496f0d09ed16cac585f438c495ed
itamarattali <itamarattali@gmail.com>
2025-01-21 22:52:10 +0200

    update paths on getPostsBySender

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 
 router.post('/', postController.addPost);
-router.get('/', postController.getAllPosts);
-router.get('/by-sender', postController.getPostsBySender);
+router.get('/', (req, res) => {
+    if (req.body.sender) {
+        return postController.getPostsBySender(req, res);
+    } else {
+        return postController.getAllPosts(req, res);
+    }
+});
 router.get('/:id', postController.getPostById);
 
 module.exports = router;
